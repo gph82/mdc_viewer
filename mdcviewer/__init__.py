5212992dbc84fc4582c22b41d50cc98306a49947
gph82 <guille.perez.hernandez@gmail.com>
2021-02-03 18:28:03 +0100

    missing inits

---
+++ mdcviewer/__init__.py
@@ -0,0 +1,14 @@
+r"""
+Welcome to mdcviewer! Below you will find the submodules exposed the API
+
+.. autosummary::
+   :toctree: generated/
+
+   io
+   ui
+   plots
+
+"""
+
+from . import io
+from . import ui
---
+++ mdcviewer/ui/__init__.py
@@ -0,0 +1,11 @@
+r"""
+Welcome to io! Below you will find the submodules exposed the API
+
+.. autosummary::
+   :toctree: generated/
+
+   CG2_accordion
+   general_options
+"""
+
+from .mdcv_ui import *
